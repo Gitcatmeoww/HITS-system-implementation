1545a7a20b9c7dc4aa70719bb49c3c0d378677ca
Gitcatmeoww <queenie.lin0323@gmail.com>
2024-09-15 21:39:32 -0700

    insert eval data into database

--- backend/app/db/connect_db.py
+++ backend/app/db/connect_db.py
@@ -1,11 +1,15 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_db_connection():
     try:
         connection = psycopg2.connect(
-            dbname=os.getenv('DB_NAME'),
+            # dbname=os.getenv('MOCK_DB_NAME'),
+            dbname=os.getenv('EVAL_DB_NAME'),
             user=os.getenv('DB_USER'),
             password=os.getenv('DB_PASSWORD'),
             host=os.getenv('DB_HOST'),
